9f106205d3c9be9470f4651eb935cbd8ebfcdc6d
kulbandita <kulbandita@gmail.com>
2017-08-10 14:09:49 +0700

    create 10 test cases

--- CheapestOrder/src/test/java/CheapestOrderTest.java
+++ CheapestOrder/src/test/java/CheapestOrderTest.java
@@ -1,2 +1,123 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CheapestOrderTest {
+    CheapestOrder cheapestOrder = new CheapestOrder();
+
+    @Test
+    @Ignore
+    public void order_A1() {
+        cheapestOrder.addItem("A");
+        double expectedValue = 100;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A1_B1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        double expectedValue = 190;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A1_B1_C1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        cheapestOrder.addItem("C");
+        double expectedValue = 270;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A1_B1_C1_D1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        cheapestOrder.addItem("C");
+        cheapestOrder.addItem("D");
+        double expectedValue = 320;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A1_B1_C1_D1_E1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        cheapestOrder.addItem("C");
+        cheapestOrder.addItem("D");
+        cheapestOrder.addItem("E");
+        double expectedValue = 375;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A2() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("A");
+        double expectedValue = 200;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A2_B1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        double expectedValue = 290;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A2_B1_C1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        cheapestOrder.addItem("C");
+        double expectedValue = 370;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A2_B1_C1_D1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        cheapestOrder.addItem("C");
+        cheapestOrder.addItem("D");
+        double expectedValue = 420;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
+
+    @Test
+    @Ignore
+    public void order_A2_B1_C1_D1_E1() {
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("A");
+        cheapestOrder.addItem("B");
+        cheapestOrder.addItem("C");
+        cheapestOrder.addItem("D");
+        cheapestOrder.addItem("E");
+        double expectedValue = 640;
+        double actualValue = cheapestOrder.getCheapestPrice();
+        assertEquals(expectedValue, actualValue, 0);
+    }
 }
